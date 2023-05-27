7f70e3f236dbbd05f1545cc34d55f8fc1159585e
TakMishima01 <114778285+TakMishima01@users.noreply.github.com>
2023-05-27 15:03:46 +0900

    "[Add]演習問題5"

--- src/Ex1_04_1.java
+++ src/Ex1_04_1.java
@@ -1,14 +1,16 @@
 class Ex1_04_1 {
 	public static void main (String[] args) {
-		for(int i = 1 ; i <= 9  ; i++) {
+		for(int i = 1 ; i <= 9  ; i++){
 			String disp3 = "";
-			for(int j = 1 ; j <= 9  ; j++) {
+			for(int j = 1 ; j <= 9  ; j++){
 				disp3 = disp3 + i*j + " ";
-			} if(args.length == 1) {
-				if(i == Integer.parseInt(args[0])) {
+				}
+			if(args.length == 1){
+				if(i == Integer.parseInt(args[0])){
 					System.out.println(disp3);
 				}
-			}else{
+			}
+			else{
 				System.out.println(disp3);
 			}
 		}
